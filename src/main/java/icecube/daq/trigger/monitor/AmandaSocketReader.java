abe6dbebed6fc5108f9cb94142d030a3f0078ee2
Pat Toale <toale@phys.psu.edu>
2007-03-19 16:17:18 -0500

    Finished socket simulator

--- src/main/java/icecube/daq/trigger/monitor/AmandaSocketReader.java
+++ src/main/java/icecube/daq/trigger/monitor/AmandaSocketReader.java
@@ -8,6 +8,8 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.net.InetAddress;
 import java.io.IOException;
+import java.io.DataInputStream;
+import java.nio.ByteBuffer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,19 +23,23 @@ public class AmandaSocketReader {
 
     private static final String DEFAULT_HOSTNAME = "localhost";
     private static final int DEFAULT_PORT = 12345;
-    private static final int DEFAULT_NBYTES = 72*128;
+    private static final int DEFAULT_NBUFF = 100;
 
     private InetAddress host = null;
     private final int port;
-    private final int nBytes;
+    private final int nBuff;
+
+    private Socket socket;
+    private DataInputStream input;
+    private ByteBuffer buffer = ByteBuffer.allocate(72*128);
 
     public AmandaSocketReader() {
-        this(DEFAULT_HOSTNAME, DEFAULT_PORT, DEFAULT_NBYTES);
+        this(DEFAULT_HOSTNAME, DEFAULT_PORT, DEFAULT_NBUFF);
     }
 
-    public AmandaSocketReader(String hostname, int port, int nBytes) {
+    public AmandaSocketReader(String hostname, int port, int nBuff) {
         this.port = port;
-        this.nBytes = nBytes;
+        this.nBuff = nBuff;
         try {
             host = InetAddress.getByName(hostname);
         } catch (UnknownHostException e) {
@@ -42,11 +48,19 @@ public class AmandaSocketReader {
     }
 
     public void connect() throws IOException {
-        Socket socket = new Socket(host, port);
+        socket = new Socket(host, port);
+        input = new DataInputStream(socket.getInputStream());
     }
 
-    public void read() {
+    public void read() throws IOException {
+        for (int i=0; i<nBuff; i++) {
+            log.info("Reading buffer " + i);
+            input.readFully(buffer.array());
+        }
+    }
 
+    public void close() throws IOException {
+        socket.close();
     }
 
     public static void main(String[] args) {
@@ -55,19 +69,20 @@ public class AmandaSocketReader {
 
         String hostname = DEFAULT_HOSTNAME;
         int port = DEFAULT_PORT;
-        int nBytes = DEFAULT_NBYTES;
+        int nBuff = DEFAULT_NBUFF;
         if (args.length < 3) {
             usage();
         } else {
             hostname = args[0];
             port = Integer.parseInt(args[1]);
-            nBytes = Integer.parseInt(args[2]);
+            nBuff = Integer.parseInt(args[2]);
         }
 
-        AmandaSocketReader reader = new AmandaSocketReader(hostname, port, nBytes);
+        AmandaSocketReader reader = new AmandaSocketReader(hostname, port, nBuff);
         try {
             reader.connect();
             reader.read();
+            reader.close();
         } catch (IOException e) {
             log.fatal("Unable to connect: ", e);
         }
--- src/main/java/icecube/daq/trigger/monitor/AmandaSocketSimulator.java
+++ src/main/java/icecube/daq/trigger/monitor/AmandaSocketSimulator.java
@@ -3,10 +3,22 @@ package icecube.daq.trigger.monitor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import icecube.icebucket.logging.LoggingConsumer;
+import icecube.daq.trigger.impl.TriggerRequestPayload;
+import icecube.daq.trigger.impl.TriggerRequestPayloadFactory;
+import icecube.daq.trigger.IReadoutRequest;
+import icecube.daq.payload.ISourceID;
+import icecube.daq.payload.IUTCTime;
+import icecube.daq.payload.splicer.Payload;
+import icecube.daq.payload.impl.SourceID4B;
+import icecube.daq.payload.impl.UTCTime8B;
 
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
+import java.io.DataOutputStream;
+import java.util.Random;
+import java.util.Vector;
+import java.nio.ByteBuffer;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,26 +26,34 @@ import java.io.IOException;
  * Date: Mar 19, 2007
  * Time: 2:18:53 PM
  */
-public class AmandaSocketSimulator implements Runnable {
-
+public class AmandaSocketSimulator
+        implements Runnable {
 
     private static final Log log = LogFactory.getLog(AmandaSocketSimulator.class);
 
     private static final int DEFAULT_PORT = 12345;
-    private static final int DEFAULT_NTRIGS = 128*100;
+    private static final double DEFAULT_RATE = 200;
 
-    private final int nTrigs;
+    private final double rate;
     private final ServerSocket server;
 
     private boolean running = false;
+
+    private int count = 0;
+    private long lastTime = 0;
+    private Random random;
+    private TriggerRequestPayloadFactory triggerFactory = new TriggerRequestPayloadFactory();
+    private long waitTime;
+    private ByteBuffer buffer = ByteBuffer.allocate(128*72);
 
     public AmandaSocketSimulator() throws IOException {
         this(DEFAULT_PORT);
     }
 
     public AmandaSocketSimulator(int port) throws IOException {
-        nTrigs = DEFAULT_NTRIGS;
-
+        rate = DEFAULT_RATE;
+        waitTime = (long) (1000*128/rate);
+        random = new Random(System.currentTimeMillis());
         server = new ServerSocket(port);
     }
 
@@ -64,9 +84,11 @@ public class AmandaSocketSimulator implements Runnable {
         while (running) {
             // Wait for connection
             Socket socket = null;
+            DataOutputStream output = null;
             try {
                 log.info("Wait for connection...");
                 socket = server.accept();
+                output = new DataOutputStream(socket.getOutputStream());
             } catch (IOException e) {
                 log.error("Error accepting connection: ", e);
             }
@@ -75,9 +97,21 @@ public class AmandaSocketSimulator implements Runnable {
 
                 // Send triggers
                 int nSent = 0;
-                while (running && (nSent < nTrigs)) {
+                while (running) {
+                    fillBuffer();
+                    try {
+                        output.write(buffer.array());
+                    } catch (IOException e) {
+                        log.error("Error writing to output stream: ", e);
+                        break;
+                    }
                     log.info("Sent " + nSent);
                     nSent++;
+                    try {
+                        Thread.sleep(waitTime);
+                    } catch (InterruptedException e) {
+                        // interrupted
+                    }
                 }
 
                 // Close connection
@@ -89,6 +123,62 @@ public class AmandaSocketSimulator implements Runnable {
             }
         }
 
+    }
+
+    /**
+     * Generate a random 9 bit integer (not including 0)
+     * @return random mask
+     */
+    private int generateTriggerMask() {
+        return (1 + random.nextInt(512));
+    }
+
+    /**
+     * Generate a delta t (in 1/10 nanoseconds) based on the rate
+     * @return waiting time in DAQ units
+     */
+    private long generateDelta() {
+        double toss;
+        do {
+            toss = random.nextDouble();
+        } while (toss <= 0.0);
+        return (long) (-1e10*Math.log(toss)/rate);
+    }
+
+    /**
+     * Generate the next TriggerRequestPayload
+     * @return a trigger payload
+     */
+    private TriggerRequestPayload generateTrigger() {
+
+        int triggerType = 0;
+        int configId = generateTriggerMask();
+        ISourceID sourceId = new SourceID4B(10000);
+        long nextTime = lastTime + generateDelta();
+        IUTCTime time = new UTCTime8B(nextTime);
+        Vector payloads = new Vector();
+        Vector readouts = new Vector();
+        IReadoutRequest readout = TriggerRequestPayloadFactory.createReadoutRequest(sourceId, count, readouts);
+        Payload trigger = triggerFactory.createPayload(count, triggerType, configId, sourceId, time, time, payloads,readout);
+        count++;
+        lastTime = nextTime;
+
+        return (TriggerRequestPayload) trigger;
+    }
+
+    private void fillBuffer() {
+        buffer.clear();
+        for (int i=0; i<128; i++) {
+            int offset = i*72;
+            TriggerRequestPayload trigger = generateTrigger();
+            log.info("Trigger size = " + trigger.getPayloadLength());
+            try {
+                trigger.writePayload(offset, buffer);
+            } catch (IOException e) {
+                log.error("Error writing to buffer: ", e);
+            }
+        }
+        buffer.flip();
     }
 
     public static void main(String[] args) {
@@ -119,5 +209,6 @@ public class AmandaSocketSimulator implements Runnable {
         log.info("Usage: AmandaSocketSimulator [port]");
         System.exit(-1);
     }
+
 
 }
