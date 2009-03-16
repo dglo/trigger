241780807471166e15029c32ba9c22b1208af3b9
Pat Toale <toale@phys.psu.edu>
2009-03-16 06:47:11 -0500

    Reimplemented the HitFilter per issue 2943

--- src/main/java/icecube/daq/trigger/config/DomSetFactory.java
+++ src/main/java/icecube/daq/trigger/config/DomSetFactory.java
@@ -1,13 +1,3 @@
-package icecube.daq.trigger.config;
-
-//import icecube.daq.trigger.config.db.DomIdLocal;
-//import icecube.daq.trigger.config.db.DomSetLocal;
-//import icecube.daq.trigger.config.db.DomSetNameLocal;
-
-//import javax.naming.Context;
-//import javax.naming.InitialContext;
-//import javax.naming.NamingException;
-
 /**
  * Created by IntelliJ IDEA.
  * User: pat
@@ -16,17 +6,41 @@ package icecube.daq.trigger.config;
  *
  *
  * Factory object with static method for constructing a DomSet based on
- * an id. Retrieves list of doms from database based on domSetId and contructs
+ * an id. Uses the DOMRegistry to get list of domIds and contructs
  * a DomSet.
  *
  */
+
+package icecube.daq.trigger.config;
+
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import icecube.daq.util.DOMRegistry;
+import icecube.daq.util.DeployedDOM;
+
+
 public class DomSetFactory
 {
 
     /**
-     * JNDI context
+     * logging object
      */
-    //private static InitialContext jndiContext;
+    private static final Log log = LogFactory.getLog(DomSetFactory.class);
+
+    /**
+     * DOMRegistry
+     */
+    private static DOMRegistry domRegistry = null;
+
+    public static void setDomRegistry(DOMRegistry dr) {
+	domRegistry = dr;
+    }
+
 
     /**
      * Get a DomSet based on this domSetId
@@ -35,51 +49,210 @@ public class DomSetFactory
      */
     public static DomSet getDomSet(int domSetId) {
 
-        // if we hava no JNDI, return null
-        //setupJNDI();
-        //if (null == jndiContext) return null;
-
-        // Look up domSetName from the DomSetNameBean
-        //Integer id = new Integer(domSetId);
-        //DomSetNameLocal domSetNameLocal = DomSetNameLocal.findByDomSetId(jndiContext, id);
-        //String domSetName = domSetNameLocal.getDomSetName();
-
-        // Look up domPk's from DomSetBean
-        //DomSetLocal[] domSet = DomSetLocal.findByDomSetId(jndiContext, id);
-        //List domIds = new ArrayList();
-        //for (int i=0; i<domSet.length; i++) {
-        //    Integer domPk = domSet[i].getDomPk();
-        //    DomIdLocal domIdLocal = DomIdLocal.findByDomPk(jndiContext, domPk);
-        //    domIds.add(domIdLocal.getDomId());
-        //}
-
-        // Create and return a shiny new domSet
-        //return new DomSet(domSetName, domIds);
+	// check the DOMRegistry
+	if (domRegistry == null) {
+	    log.warn("Must set the DOMRegistry first");
 	    return null;
+	}
+
+	if (domSetId == 0) {
+	    // AMANDA SYNC (0,91)
+	    boolean good = false;
+	    List domIds = new ArrayList();
+
+	    Set<DeployedDOM> doms = domRegistry.getDomsOnString(0);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if (pos == 91) {
+		    // this is our baby
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		    good = true;
+		}
+	    }
+
+	    if (good) {
+		return new DomSet("AMANDA_SYNC", domIds);
+	    } else {
+		log.error("Failed to create DomSet for domSetId=0 (AMANDA_SYNC)");
+		return null;
+	    }
+
+	} else if (domSetId == 1) {
+	    // AMANDA TRIG (0,92)
+	    boolean good = false;
+	    List domIds = new ArrayList();
+
+	    Set<DeployedDOM> doms = domRegistry.getDomsOnString(0);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if (pos == 92) {
+		    // this is our baby
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		    good = true;
+		}
+	    }
+
+	    if (good) {
+		return new DomSet("AMANDA_TRIG", domIds);
+	    } else {
+		log.error("Failed to create DomSet for domSetId=1 (AMANDA_TRIG)");
+		return null;
+	    }
+
+
+	} else if (domSetId == 2) {
+	    // InIce (1,1)-(1,60):(80,1)-(80,60)
+	    List domIds = new ArrayList();
+
+	    for (int str=1; str<=80; str++) {
+		Set<DeployedDOM> doms = domRegistry.getDomsOnString(str);
+		for (DeployedDOM dom: doms) {
+		    int pos = dom.getStringMinor();
+		    if ( (pos >= 1) && (pos <= 60) ) {
+			String domId = dom.getDomId();
+			domIds.add(domId);
+		    }
+		}
+	    }
+
+	    if (!domIds.isEmpty()) {
+		return new DomSet("INICE", domIds);
+	    } else {
+		log.error("Failed to create DomSet for domSetId=2 (INICE)");
+		return null;
+	    }
+
+	} else if (domSetId == 3) {
+	    // IceTop (1,61)-(1,64):(80,61)-(80,64)
+	    List domIds = new ArrayList();
+
+	    for (int str=1; str<=80; str++) {
+		Set<DeployedDOM> doms = domRegistry.getDomsOnString(str);
+		for (DeployedDOM dom: doms) {
+		    int pos = dom.getStringMinor();
+		    if ( (pos >= 61) && (pos <= 64) ) {
+			String domId = dom.getDomId();
+			domIds.add(domId);
+		    }
+		}
+	    }
+
+	    if (!domIds.isEmpty()) {
+		return new DomSet("ICETOP", domIds);
+	    } else {
+		log.error("Failed to create DomSet for domSetId=3 (ICETOP)");
+		return null;
+	    }
+
+	} else if (domSetId == 4) {
+	    // DeepCore
+	    List domIds = new ArrayList();
+
+	    // InIce strings are 26,27,35,36,37,45,46
+	    int minPos = 41;
+	    int maxPos = 60;
+
+	    // 26
+	    Set<DeployedDOM> doms = domRegistry.getDomsOnString(26);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 27
+	    doms = domRegistry.getDomsOnString(27);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 35
+	    doms = domRegistry.getDomsOnString(35);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 36
+	    doms = domRegistry.getDomsOnString(36);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 37
+	    doms = domRegistry.getDomsOnString(37);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 45
+	    doms = domRegistry.getDomsOnString(45);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // 46
+	    doms = domRegistry.getDomsOnString(46);
+	    for (DeployedDOM dom: doms) {
+		int pos = dom.getStringMinor();
+		if ( (pos >= minPos) && (pos <= maxPos) ) {
+		    String domId = dom.getDomId();
+		    domIds.add(domId);
+		}
+	    }
+
+	    // DeepCore strings are 81-86
+	    minPos = 11;
+	    maxPos = 60;
+
+	    for (int str=81; str<=86; str++) {
+		doms = domRegistry.getDomsOnString(str);
+		for (DeployedDOM dom: doms) {
+		    int pos = dom.getStringMinor();
+		    if ( (pos >= minPos) && (pos <= maxPos) ) {
+			String domId = dom.getDomId();
+			domIds.add(domId);
+		    }
+		}
+	    }
+
+	    if (!domIds.isEmpty()) {
+		return new DomSet("DEEPCORE", domIds);
+	    } else {
+		log.error("Failed to create DomSet for domSetId=4 (DEEPCORE)");
+		return null;
+	    }
+
+	} else {
+
+	    log.error("Invalid DomSetId! must be 0,1,2,3,or 4");
+	    return null;
+
+	}
+
     }
-
-    /*
-    private static void setupJNDI() {
-
-        Properties prop = new Properties();
-        String jndiHost = "";
-        String jndiPort = "";
-
-        prop.put(Context.INITIAL_CONTEXT_FACTORY,
-                 "org.jnp.interfaces.NamingContextFactory");
-        prop.put(Context.PROVIDER_URL,
-                 "jnp://" + jndiHost + ":" + jndiPort);
-        prop.put(Context.URL_PKG_PREFIXES,
-                 "org.jboss.naming:org.jnp.interfaces");
-
-        try {
-            jndiContext = new InitialContext(prop);
-        } catch (NamingException e) {
-            jndiContext = null;
-            log.warn("No access to JNDI, cannot create DomSet for HitFilter");
-        }
-
-    }
-    */
 
 }
