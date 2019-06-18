f530853811b2787914f7d2d0b37a51cbc5b2f13f
Dave Glowacki <dglo@icecube.wisc.edu>
2019-06-18 11:40:43 -0500

    Issue 8681: Improve handling of null SnDAQ alerter

--- src/main/java/icecube/daq/trigger/control/TriggerCollector.java
+++ src/main/java/icecube/daq/trigger/control/TriggerCollector.java
@@ -400,18 +400,30 @@ class CollectorThread
     @Override
     public long getSNDAQAlertsDropped()
     {
+        if (alerter == null) {
+            return 0L;
+        }
+
         return alerter.getNumDropped();
     }
 
     @Override
     public int getSNDAQAlertsQueued()
     {
+        if (alerter == null) {
+            return 0;
+        }
+
         return alerter.getNumQueued();
     }
 
     @Override
     public long getSNDAQAlertsSent()
     {
+        if (alerter == null) {
+            return 0L;
+        }
+
         return alerter.getNumSent();
     }
 
@@ -439,6 +451,11 @@ class CollectorThread
 
     private void initializeSNDAQAlerter(List<ITriggerAlgorithm> algorithms)
     {
+        if (alerter != null) {
+            LOG.error("Attempted to create an existing SnDAQ alerter");
+            return;
+        }
+
         try {
             alerter = new SNDAQAlerter(algorithms);
         } catch (AlertException ae) {
@@ -451,6 +468,10 @@ class CollectorThread
 
     private void notifySNDAQ(List<ITriggerRequestPayload> list)
     {
+        if (alerter == null) {
+            LOG.error("Alerter " + alerter + " has not been initialized");
+        }
+
         if (!alerter.isActive()) {
             LOG.error("Alerter " + alerter + " is not active");
             return;
