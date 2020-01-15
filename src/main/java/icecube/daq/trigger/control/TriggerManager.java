abb2df93a41e0cc85bfe489d914f2c4065ce794b
Dave Glowacki <dglo@icecube.wisc.edu>
2020-01-15 08:33:20 -0600

    Issue 8759: Use SubscribedList.isEmpty() where appropriate

--- src/main/java/icecube/daq/trigger/control/TriggerManager.java
+++ src/main/java/icecube/daq/trigger/control/TriggerManager.java
@@ -253,7 +253,7 @@ public class TriggerManager
     @Override
     public void analyze(List<Spliceable> splicedObjects)
     {
-        if (queueList.getNumSubscribers() <= 0) {
+        if (queueList.isEmpty()) {
             throw new Error("No consumers for " + splicedObjects.size() +
                             " hits");
         }
@@ -310,7 +310,7 @@ public class TriggerManager
     @Override
     public void flush()
     {
-        if (queueList.getNumSubscribers() > 0) {
+        if (!queueList.isEmpty()) {
             try {
                 queueList.push(FLUSH_PAYLOAD);
             } catch (Error err) {
@@ -991,13 +991,13 @@ public class TriggerManager
     @Override
     public void unsubscribeAll()
     {
-        if (queueList.getNumSubscribers() > 0) {
+        if (!queueList.isEmpty()) {
             for (ITriggerAlgorithm a : algorithms) {
                 a.unsubscribe(queueList);
                 a.resetAlgorithm();
             }
 
-            if (queueList.getNumSubscribers() > 0) {
+            if (!queueList.isEmpty()) {
                 LOG.error(String.format("SubscribedList still has %d entries",
                                         queueList.size()));
             }
