b071dc55ecb1471c359fb6b820c0b3a98196c63e
Dave Glowacki <dglo@icecube.wisc.edu>
2020-03-06 06:20:31 -0600

    Temporarily revert reversion warning for Akupara_rc1

--- src/main/java/icecube/daq/trigger/algorithm/AbstractTrigger.java
+++ src/main/java/icecube/daq/trigger/algorithm/AbstractTrigger.java
@@ -48,6 +48,9 @@ public abstract class AbstractTrigger
     /** Log object for this class */
     private static final Logger LOG = Logger.getLogger(AbstractTrigger.class);
 
+    /** If true, print a warning whenever the earliest time goes backward */
+    private static final boolean WARN_TIME_BACKWARD = false;
+
     /** Requests can be up to this number of DAQ ticks wide */
     private static final long REQUEST_WIDTH = 100000000;
 
@@ -1050,7 +1053,9 @@ public abstract class AbstractTrigger
             {
                 long diff = earliestPayloadOfInterest.getUTCTime() -
                     payload.getUTCTime();
-                LOG.error("Earliest time went " + diff + " ticks backward");
+                if (WARN_TIME_BACKWARD) {
+                    LOG.error("Earliest time went " + diff + " ticks backward");
+                }
             }
 
             earliestPayloadOfInterest = payload;
